8d2edd8c32588d6e0657f14106af7808bce5d989
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-11 12:59:16 +0200

    add team controller

---
+++ src/main/java/com/jakuza/projects/controller/TeamController.java
@@ -0,0 +1,38 @@
+package com.jakuza.projects.controller;
+
+import java.util.List;
+
+import com.jakuza.projects.model.Team;
+import com.jakuza.projects.service.TeamService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* TeamController
+*/
+@RestController
+@RequestMapping("/teams")
+public class TeamController {
+
+	private final TeamService teamService;
+
+	public TeamController(TeamService teamService){
+		this.teamService = teamService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Team>> getTeams(){
+		return ResponseEntity.ok().body(teamService.getAll());
+	}
+
+	@PostMapping
+	public ResponseEntity<Team> addTeam(@RequestBody Team team){
+		return ResponseEntity.ok().body(teamService.addTeam(team));
+	}
+	
+}
