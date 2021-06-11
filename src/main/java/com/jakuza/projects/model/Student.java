433caa0da2990161a3d23b154e578f5b53e9a0e9
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-11 10:47:29 +0200

    set up relationship between student and team

--- src/main/java/com/jakuza/projects/model/Student.java
+++ src/main/java/com/jakuza/projects/model/Student.java
@@ -1,17 +1,27 @@
 package com.jakuza.projects.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
 * Student
 */
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Student {
@@ -23,5 +33,10 @@ public class Student {
 	private String name;
 
 	private String email;
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "students")
+	private Set<Team> teams = new HashSet<>();
+		
 	
 }
--- src/main/java/com/jakuza/projects/model/Team.java
+++ src/main/java/com/jakuza/projects/model/Team.java
@@ -1,17 +1,27 @@
 package com.jakuza.projects.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
 * Team
 */
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Team {
@@ -21,6 +31,14 @@ public class Team {
 	private Long id;
 
 	private String name;
+
+	@ManyToMany
+	@JoinTable(
+		name = "student_team",
+		joinColumns = @JoinColumn(name = "team_id"),
+		inverseJoinColumns = @JoinColumn(name = "student_id")
+	)
+	private Set<Student> students = new HashSet<>();
 
 	
 }
