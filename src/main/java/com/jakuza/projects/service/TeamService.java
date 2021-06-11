77d04c3d02692dea892c9da9dd2b0a5d5792a295
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-11 11:25:32 +0200

    add team service

---
+++ src/main/java/com/jakuza/projects/service/TeamService.java
@@ -0,0 +1,30 @@
+package com.jakuza.projects.service;
+
+import java.util.List;
+
+import com.jakuza.projects.model.Team;
+import com.jakuza.projects.repository.TeamRepository;
+
+import org.springframework.stereotype.Service;
+
+/**
+* TeamService
+*/
+@Service
+public class TeamService {
+		
+	private final TeamRepository repository;
+
+	public TeamService(TeamRepository repository){
+		this.repository = repository;
+	}
+
+	public List<Team> getTeams(){
+		return repository.findAll();
+	}
+
+	public Team addTeam(Team team){
+		return repository.save(team);
+	}
+	
+}
