4057cef55393feb584f4e9c8c9842f6d336c3f47
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-11 13:51:37 +0200

    add student to team route

--- src/main/java/com/jakuza/projects/controller/TeamController.java
+++ src/main/java/com/jakuza/projects/controller/TeamController.java
@@ -7,7 +7,9 @@ import com.jakuza.projects.service.TeamService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,18 +23,33 @@ public class TeamController {
 
 	private final TeamService teamService;
 
+	
 	public TeamController(TeamService teamService){
 		this.teamService = teamService;
 	}
 
+	
 	@GetMapping
 	public ResponseEntity<List<Team>> getTeams(){
 		return ResponseEntity.ok().body(teamService.getAll());
 	}
 
+	
 	@PostMapping
 	public ResponseEntity<Team> addTeam(@RequestBody Team team){
 		return ResponseEntity.ok().body(teamService.addTeam(team));
 	}
+
 	
+	@PutMapping("/{teamId}/student/{studentId}")
+	public ResponseEntity<Team> addStudentToTeam(
+			@PathVariable Long teamId,
+			@PathVariable Long studentId
+			){
+
+		return ResponseEntity.ok().body(teamService.addStudentToTeam(teamId, studentId)) ;
+
+	}
+
+
 }
