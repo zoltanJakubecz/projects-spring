8b2951d1bbf633eeb6ef735602062ffdce2a35f4
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-12 09:14:22 +0200

    extend location api with update and delete

--- src/main/java/com/jakuza/projects/controller/LocationController.java
+++ src/main/java/com/jakuza/projects/controller/LocationController.java
@@ -6,8 +6,11 @@ import com.jakuza.projects.model.Location;
 import com.jakuza.projects.service.LocationService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,8 +34,27 @@ public class LocationController {
 		return ResponseEntity.ok().body(locationService.getAll());
 	}
 
+	
 	@PostMapping
 	public ResponseEntity<Location> add(@RequestBody Location location){
 		return ResponseEntity.ok().body(locationService.add(location));
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Location> getOne(@PathVariable Long id){
+		return ResponseEntity.ok().body(locationService.getOne(id));
+	}
+
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Location> update(@PathVariable Long id, @RequestBody Location location){
+		return ResponseEntity.ok().body(locationService.update(id, location));
+	}
+
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable Long id){
+		return ResponseEntity.ok().body(locationService.remove(id));
+	}
+
 }
--- src/main/java/com/jakuza/projects/service/LocationService.java
+++ src/main/java/com/jakuza/projects/service/LocationService.java
@@ -1,6 +1,8 @@
 package com.jakuza.projects.service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import com.jakuza.projects.model.Location;
 import com.jakuza.projects.repository.LocationRepository;
@@ -27,6 +29,35 @@ public class LocationService {
 
 	public Location add(Location location){
 		return locationRepository.save(location);
+	}
+
+	public Location getOne(Long id){
+		Location location = locationRepository
+														.findById(id)
+														.orElse(null);
+		return location;
+	}
+
+	public Location update(Long id, Location location){
+		return locationRepository.findById(id)
+			.map((loc) -> {
+				loc.setName(location.getName());
+				loc.setAddress(location.getAddress());
+				return locationRepository.save(loc);
+			})
+		.orElse(null);
+	}
+
+
+	public boolean remove(Long id){
+
+		Optional<Location> location = locationRepository.findById(id);
+														
+		if(location.isPresent()){
+			locationRepository.delete(location.get());
+			return true;
+		}
+		return false;
 	}
 
 }
