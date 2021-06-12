4fc04e96da85a39c5e08e6180d2f712ce95f85e3
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-12 11:59:53 +0200

    extend project controller with update and delete

--- src/main/java/com/jakuza/projects/controller/ProjectController.java
+++ src/main/java/com/jakuza/projects/controller/ProjectController.java
@@ -6,6 +6,7 @@ import com.jakuza.projects.model.Project;
 import com.jakuza.projects.service.ProjectService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +38,24 @@ public class ProjectController {
 	@PostMapping
 	public ResponseEntity<Project> addProject(@RequestBody Project project){
 		return ResponseEntity.ok().body(projectService.add(project));
+	}
+
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Project> getOne(@PathVariable Long id){
+		return ResponseEntity.ok().body(projectService.getOne(id));
+	}
+
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Project> update(@PathVariable Long id, @RequestBody Project project){
+		return ResponseEntity.ok().body(projectService.update(id, project));
+	}
+
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable Long id){
+		return ResponseEntity.ok().body(projectService.remove(id));
 	}
 
 
