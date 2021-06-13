3f8c5e97a247ec88089e2caf3d8d5a1b12b140ae
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-13 18:48:33 +0200

    add repository tests

---
+++ src/test/java/com/jakuza/projects/repository/StudentRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.jakuza.projects.repository;
+
+import com.jakuza.projects.model.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.instrument.classloading.jboss.JBossLoadTimeWeaver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    void itShouldCheckIfStudentEmailExist(){
+        Student student = new Student();
+        student.setName("Jani");
+        student.setEmail("jani@kukutyin.hu");
+        studentRepository.save(student);
+        boolean expected = studentRepository.selectExistsEmail("jani@kukutyin.hu");
+        assertThat(expected).isTrue();
+
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExist(){
+        boolean expected = studentRepository.selectExistsEmail("jani@kukutyin.hu");
+        assertThat(expected).isFalse();
+
+    }
+
+}
