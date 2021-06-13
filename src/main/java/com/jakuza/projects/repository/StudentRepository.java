3c65c108e155452d1e24e7dabde6353bccc020dd
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-13 18:48:08 +0200

    add email isExist query

--- src/main/java/com/jakuza/projects/repository/StudentRepository.java
+++ src/main/java/com/jakuza/projects/repository/StudentRepository.java
@@ -3,11 +3,18 @@ package com.jakuza.projects.repository;
 import com.jakuza.projects.model.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
 * StudentRepository
 */
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    @Query("" +
+            "SELECT CASE WHEN COUNT(s) > 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM Student s " +
+            "WHERE s.email = ?1"
+    )
+    Boolean selectExistsEmail(String email);
 	
 }
